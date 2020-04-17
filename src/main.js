0b41ac1d4f8cad6f79bd6003871ba4531fbf8ba8
Анна Полуэктова <a.poluektova88@gmail.com>
2020-04-18 00:59:07 +0400

    Добавляет закрытие по ESC и сообщение-заглушку при пустом списке

--- src/main.js
+++ src/main.js
@@ -8,21 +8,37 @@ import ShowMoreButtonComponent from "./components/show-more-button.js";
 import ExtraFilmsComponent from "./components/extra-films.js";
 import FilmDetailsComponent from "./components/film-details.js";
 import FooterStatisticsComponent from "./components/footer-statistics.js";
+import NoFilmsComponent from "./components/no-films.js";
 import {generateFilms} from "./mock/film.js";
 import {generateFilters} from "./mock/filters.js";
 import {render, RenderPosition} from "./utils.js";
 
-const TOTAL_FILMS_AMOUNT = 20;
-const SHOWN_FILM_CARDS_AMOUNT = 5;
-const FILM_CARDS_AMOUNT_BY_BUTTON = 5;
-
-const EXTRA_FILM_CARDS_AMOUNT = 2;
+const Cards = {
+  TOTAL: 20,
+  SHOWN: 5,
+  BY_BUTTON: 5,
+  EXTRA: 2,
+};
 
 const renderFilmCard = (filmsListElement, film) => {
   const body = document.querySelector(`body`);
 
-  const onFilmCardElementClick = () => {
+  const openPopup = () => {
     body.appendChild(filmDetails.getElement());
+  };
+
+  const onFilmCardElementClick = () => {
+    openPopup();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  };
+
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      onFilmDetailsClose();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
   };
 
   const filmCard = new FilmCardComponent(film);
@@ -39,21 +55,26 @@ const renderFilmCard = (filmsListElement, film) => {
 
   const filmDetailsCloseButton = filmDetails.getElement().querySelector(`.film-details__close-btn`);
 
-  const onFilmDetailsCloseButtonClick = () => {
+  const onFilmDetailsClose = () => {
     body.removeChild(filmDetails.getElement());
   };
 
-  filmDetailsCloseButton.addEventListener(`click`, onFilmDetailsCloseButtonClick);
+  filmDetailsCloseButton.addEventListener(`click`, onFilmDetailsClose);
 
   render(filmsListElement, filmCard.getElement(), RenderPosition.BEFOREEND);
 };
 
 const renderFilms = (filmsComponent, films) => {
+  if (films.length === 0) {
+    render(filmsComponent.getElement(), new NoFilmsComponent().getElement(), RenderPosition.BEFOREEND);
+    return;
+  }
+
   render(filmsComponent.getElement(), new FilmsListComponent().getElement(), RenderPosition.BEFOREEND);
 
   const filmsListElement = filmsComponent.getElement().querySelector(`.films-list__container`);
 
-  let shownFilmCardsAmount = SHOWN_FILM_CARDS_AMOUNT;
+  let shownFilmCardsAmount = Cards.SHOWN;
   films.slice(0, shownFilmCardsAmount)
     .forEach((filmCard) => {
       renderFilmCard(filmsListElement, filmCard);
@@ -64,7 +85,7 @@ const renderFilms = (filmsComponent, films) => {
 
   const onShowMoreButtonClick = () => {
     const prevFilmsCount = shownFilmCardsAmount;
-    shownFilmCardsAmount = shownFilmCardsAmount + FILM_CARDS_AMOUNT_BY_BUTTON;
+    shownFilmCardsAmount = shownFilmCardsAmount + Cards.BY_BUTTON;
 
     films.slice(prevFilmsCount, shownFilmCardsAmount)
     .forEach((filmCard) => renderFilmCard(filmsListElement, filmCard));
@@ -78,7 +99,7 @@ const renderFilms = (filmsComponent, films) => {
   showMoreButton.getElement().addEventListener(`click`, onShowMoreButtonClick);
 };
 
-const films = generateFilms(TOTAL_FILMS_AMOUNT);
+const films = generateFilms(Cards.TOTAL);
 const filters = generateFilters();
 
 const siteHeader = document.querySelector(`.header`);
@@ -97,14 +118,21 @@ const renderExtraFilms = (title) => {
   render(filmsContainer.getElement(), extraFilmsComponent.getElement(), RenderPosition.BEFOREEND);
   const extraFilmsContainer = extraFilmsComponent.getElement().querySelector(`.films-list__container`);
 
-  const extraFilmCards = generateFilms(EXTRA_FILM_CARDS_AMOUNT);
+  const extraFilmCards = generateFilms(Cards.EXTRA);
   extraFilmCards.forEach((filmCard) => {
     renderFilmCard(extraFilmsContainer, filmCard);
   });
 };
 
-renderExtraFilms(`Top Rated`);
-renderExtraFilms(`Most Commented`);
+if (films.length !== 0) {
+  renderExtraFilms(`Top Rated`);
+  renderExtraFilms(`Most Commented`);
+}
+
 
 const siteFooter = document.querySelector(`.footer`);
-render(siteFooter, new FooterStatisticsComponent(TOTAL_FILMS_AMOUNT).getElement(), RenderPosition.BEFOREEND);
+if (films.length !== 0) {
+  render(siteFooter, new FooterStatisticsComponent(Cards.TOTAL).getElement(), RenderPosition.BEFOREEND);
+} else {
+  render(siteFooter, new FooterStatisticsComponent(0).getElement(), RenderPosition.BEFOREEND);
+}
