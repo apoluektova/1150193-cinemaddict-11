4662edd07ac3d54d0afabd87964846935bd9b38d
Анна Полуэктова <a.poluektova88@gmail.com>
2020-04-28 17:42:51 +0400

    Настраивает датабиндинг

---
+++ src/controllers/movie-controller.js
@@ -0,0 +1,121 @@
+import FilmCardComponent from "../components/film-card.js";
+import FilmDetailsComponent from "../components/film-details.js";
+import {render, openPopup, remove, replace, RenderPosition} from "../utils/render.js";
+
+const Mode = {
+  DEFAULT: `default`,
+  DETAILS: `details`,
+};
+
+export default class MovieController {
+  constructor(container, onDataChange, onViewChange) {
+    this._container = container;
+    this._onDataChange = onDataChange;
+    this._onViewChange = onViewChange;
+    this._mode = Mode.DEFAULT;
+    this._filmCardComponent = null;
+    this._filmDetailsComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  render(film) {
+    const oldFilmCardComponent = this._filmCardComponent;
+    const oldFilmDetailsComponent = this._filmDetailsComponent;
+
+    this._filmCardComponent = new FilmCardComponent(film);
+    this._filmDetailsComponent = new FilmDetailsComponent(film);
+    // this._body = document.querySelector(`body`);
+
+    const onFilmCardElementClick = () => {
+      this._mode = Mode.DETAILS;
+      openPopup(document.body, this._filmDetailsComponent);
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    };
+
+    this._filmCardComponent.setPosterClickHandler(() => {
+      this._onViewChange();
+      onFilmCardElementClick();
+    });
+
+    this._filmCardComponent.setTitleClickHandler(() => {
+      this._onViewChange();
+      onFilmCardElementClick();
+    });
+
+    this._filmCardComponent.setCommentsClickHandler(() => {
+      this._onViewChange();
+      onFilmCardElementClick();
+    });
+
+    this._filmCardComponent.setWatchedButtonClickHandler(() => {
+      this._onDataChange(film, Object.assign({}, film, {
+        alreadyWatched: !film.alreadyWatched,
+      }));
+    });
+
+    this._filmCardComponent.setWatchlistButtonClickHandler(() => {
+      this._onDataChange(film, Object.assign({}, film, {
+        watchlist: !film.watchlist,
+      }));
+    });
+
+    this._filmCardComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(film, Object.assign({}, film, {
+        isFavorite: !film.isFavorite,
+      }));
+    });
+
+    this._filmDetailsComponent.setCloseButtonClickHandler(() => {
+      remove(this._filmDetailsComponent);
+      this._mode = Mode.DETAILS;
+    });
+
+    this._filmDetailsComponent.setWatchedButtonClickHandler(() => {
+      this._onDataChange(film, Object.assign({}, film, {
+        alreadyWatched: !film.alreadyWatched,
+      }));
+    });
+
+    this._filmDetailsComponent.setWatchlistButtonClickHandler(() => {
+      this._onDataChange(film, Object.assign({}, film, {
+        watchlist: !film.watchlist,
+      }));
+    });
+
+    this._filmDetailsComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(film, Object.assign({}, film, {
+        isFavorite: !film.isFavorite,
+      }));
+    });
+
+    this._filmDetailsComponent.setEmojiClickHandler((evt) => {
+      const currentEmoji = evt.target.value;
+      const emojiContainer = document.querySelector(`.film-details__add-emoji-label`);
+      emojiContainer.innerHTML = `<img src="images/emoji/${currentEmoji}.png" width="55" height="55" alt="emoji-${currentEmoji}">`;
+    });
+
+    if (oldFilmDetailsComponent && oldFilmCardComponent) {
+      replace(this._filmCardComponent, oldFilmCardComponent);
+      replace(this._filmDetailsComponent, oldFilmDetailsComponent);
+    } else {
+      render(this._container, this._filmCardComponent, RenderPosition.BEFOREEND);
+    }
+  }
+
+  setDefaultView() {
+    if (this._mode !== Mode.DEFAULT) {
+      remove(this._filmDetailsComponent);
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      remove(this._filmDetailsComponent);
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+}
