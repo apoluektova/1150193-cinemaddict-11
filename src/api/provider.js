b010ad78e7afcfc371646469c0bd478484251701
Анна Полуэктова <a.poluektova88@gmail.com>
2020-06-01 00:06:24 +0400

    Реализует оффлайн режим

--- src/api/provider.js
+++ src/api/provider.js
@@ -1,21 +1,88 @@
+import Film from "../models/film";
+
+const isOnline = () => {
+  return window.navigator.onLine;
+};
+
+const getSyncedFilms = (items) => {
+  return items.filter(({success}) => success)
+    .map(({payload}) => payload.movie);
+};
+
+const createStoreStructure = (items) => {
+  return items.reduce((acc, current) => {
+    return Object.assign({}, acc, {
+      [current.id]: current,
+    });
+  }, {});
+};
+
 export default class Provider {
-  constructor(api) {
+  constructor(api, store) {
     this._api = api;
+    this._store = store;
   }
 
   getFilms() {
-    return this._api.getFilms();
+    if (isOnline()) {
+      return this._api.getFilms()
+        .then((films) => {
+          const items = createStoreStructure(films.map((film) => film.toRAW()));
+
+          this._store.setItems(items);
+
+          return films;
+        });
+    }
+
+    const storeFilms = Object.values(this._store.getItems());
+    return Promise.resolve(Film.parseMultiple(storeFilms));
   }
 
   updateFilm(id, film) {
-    return this._api.updateFilm(id, film);
+    if (isOnline()) {
+      return this._api.updateFilm(id, film)
+        .then((newFilm) => {
+          this._store.setItem(newFilm.id, newFilm.toRAW());
+
+          return newFilm;
+        });
+    }
+    const localFilm = Film.clone(Object.assign(film, {id}));
+    this._store.setItem(id, localFilm.toRAW());
+    return Promise.resolve(localFilm);
   }
 
   createComment(id, comment) {
-    return this._api.createComment(id, comment);
+    if (isOnline()) {
+      return this._api.createComment(id, comment);
+    }
+
+    return Promise.reject(`offline logic is not implemented`);
   }
 
   deleteComment(id) {
-    return this._api.deleteComment(id);
+    if (isOnline()) {
+      return this._api.deleteComment(id);
+    }
+
+    return Promise.reject(`offline logic is not implemented`);
+  }
+
+  sync() {
+    if (isOnline()) {
+      const storeFilms = Object.values(this._store.getItems());
+
+      return this._api.sync(storeFilms)
+        .then((response) => {
+          const updatedFilms = getSyncedFilms(response.updated);
+
+          const items = createStoreStructure([...updatedFilms]);
+
+          this._store.setItems(items);
+        });
+    }
+
+    return Promise.reject(new Error(`Sync data failed`));
   }
 }
--- src/main.js
+++ src/main.js
@@ -9,13 +9,18 @@ import PageController from "./controllers/page-controller.js";
 import Provider from "./api/provider.js";
 import SortingComponent from "./components/sorting.js";
 import StatisticsComponent from "./components/statistics.js";
+import Store from "./api/store.js";
 import {render, RenderPosition, remove} from "./utils/render.js";
 
-const AUTHORIZATION = `Basic asClkjaldhfsoklnQN`;
+const AUTHORIZATION = `Basic asClkldhfsoklnQN`;
 const END_POINT = `https://11.ecmascript.pages.academy/cinemaddict`;
+const STORE_PREFIX = `cinemaddict-localstorage`;
+const STORE_VER = `v1`;
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 
 const api = new API(END_POINT, AUTHORIZATION);
-const apiWithProvider = new Provider(api);
+const store = new Store(STORE_NAME, window.localStorage);
+const apiWithProvider = new Provider(api, store);
 const filmsModel = new FilmsModel();
 const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer__statistics`);
@@ -58,3 +63,20 @@ apiWithProvider.getFilms()
      remove(loadingComponent);
      render(siteMainElement, new NoFilmsComponent(), RenderPosition.BEFOREEND);
    });
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+      .then(() => {
+      }).catch(() => {
+      });
+});
+
+window.addEventListener(`online`, () => {
+  document.title = document.title.replace(` [offline]`, ``);
+
+  apiWithProvider.sync();
+});
+
+window.addEventListener(`offline`, () => {
+  document.title = document.title.concat(` [offline]`);
+});
